3609dfadb8f2516e8732a0e8d5bfa3f8c462b29f
clgtj91 <clgjt97@gmail.com>
2018-11-03 13:33:33 -0700

    no google sheets

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.set('view engine','ejs'); // setting default engine to be EJS, express auto 
 app.get('/',(req,res) => {
     const data = {
         person: {
-            firstN: 'Cesar',
-            lastN: 'Leyva'
+            firstN: "Cesar",
+            lastN: "Leyva"
         }
     }
 
@@ -27,54 +27,16 @@ app.get('/contact',(req, res) => {
     res.render('contact');
  });
 
- /*
- app.post('https://sheets.googleapis.com/v4/spreadsheets/1ovHnMffenM-DvRdyvCtKbPaMLU9Luxa4KktgRTevVEA/values/'contacts'?valueInputOption=RAW', (req, res, next) => {
-    var firstN = document.getElementById('firstName');
-    var lastN = document.getElementById('lastName');
-    var eMail = document.getElementById('email');
-
-    {
-        "range": "contacts",
-        "majorDimension": "COLUMNS",
-        "values": [
-          [null,firstN, null],
-          [null,lastN, null],
-          [null,eMail, null]
-        ]
-      }
-
- });
-
- */
-app.locals({
-    scripts: [],
-  renderScriptsTags: function (all) {
-    app.locals.scripts = [];
-    if (all != undefined) {
-      return all.map(function(script) {
-        return '<script src="/javascripts/' + script + '"></script>';
-      }).join('\n ');
-    }
-    else {
-      return '';
-    }
-  },
-  getScripts: function(req, res) {
-    return scripts;
-  }
-});
-  
-  app.dynamicHelpers({
-    scripts: function(req, res) {
-      return ['jquery-3.3.1.min.js'];
-    }
-  });
-
 app.post('/thanks', (req, res) => { 
-    res.render('thanks',{ insert_value: req.body });
+    res.render('thanks',{ contact: req.body });
+    console.log({ contact: req.body });
  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
+
+
+
+   
